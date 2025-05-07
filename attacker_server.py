15527bf6b5df4c9ed23898271ccf1941a3660878
Qwerty Jacob <jesusfcevallos@gmail.com>
2025-05-07 16:37:45 +0200

    refac

--- attacker_server.py
+++ attacker_server.py
@@ -221,16 +221,16 @@ async def start_replay(kwargs: dict):
     
     replay_thread, checker_thread = start_replay_with_monitor()  # Execute the function immediately
 
-    return {"status": "Replay started", "pattern": PATTERN_TO_REPLAY, "target": TARGET_IP}
+    return {"message": f"Replay started pattern {PATTERN_TO_REPLAY} target {TARGET_IP}"}
 
 
 @app.get("/replay_status")
 async def get_replay_status():
     logger.info("Replay status endpoint called")
     if current_replay_process is None:
-        return {"status": "stopped"}
+        return {"message": "Replay not running"}
     else:
-        return {"status": "running"}
+        return {"message": "Replay running"}
 
 
 @app.post("/stop")
